6adb4e3fd5ca8a19428450f27a913c1253966f7a
Louis Labeyrie <labeyrielouis@gmail.com>
2024-12-16 11:11:25 +0100

    feat: add pino logging and update server port configuration
    Signed-off-by: Louis Labeyrie <labeyrielouis@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { pinoHttp } from 'pino-http';
+import pino from 'pino';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+
+  const logger = pino({
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+      },
+    },
+  });
+
+  app.use(pinoHttp({ logger }));
+
+  await app.listen(port, () => {
+    console.log(`🚀 Server ready at: http://localhost:${port}`);
+  });
 }
 bootstrap();
